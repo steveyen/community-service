b2982d4a70a231f4c82b68058ae2520b56e582ab
admin@metaha.com <admin@metaha.com>
2008-10-31 23:01:36 +0000

    whoops, migrations are generated, not supposed to be checked in

--- db/migrate/20081031222502_community_engine_to_version_60.rb
+++
@@ -1,9 +0,0 @@
-class CommunityEngineToVersion60 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["community_engine"].migrate(60)
-  end
-
-  def self.down
-    Engines.plugins["community_engine"].migrate(0)
-  end
-end
