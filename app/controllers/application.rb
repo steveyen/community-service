46062a14e7ace5f097654b7b4d5259278bdb949a
scy <scy@scy-unbuntu-01.(none)>
2008-11-03 17:55:23 -0800

    playing with community engine config

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,17 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  # Save a read-only snapshot of the community engine config.
+  #
+  @@AppConfig_initial = AppConfig.clone
+
+  before_filter :app_config_filter
+
+  private
+    def app_config_filter
+      AppConfig.community_name = "some community name"
+      print " xxx= #{@@AppConfig_initial.community_name}\n"
+      print " yyy= #{AppConfig.community_name}\n"
+    end
 end
